8bba9513350546bd744f833b7cb68bc03251e1f6
PioacaRobert <pioaca.robert014@gmail.com>
2021-09-02 16:14:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function myCalc() {
+    let a = parseInt(document.getElementById("first").value);
+    let b = parseInt(document.getElementById("secound").value);
+    let x = document.getElementById("symbol").value;
+    
+
+    
+
+    switch(x) {
+        case "+":
+            result = alert(a + b);
+            break;
+
+        case "-":
+            result = alert(a - b);
+            break;
+
+        case "*":
+            result = alert(a * b);
+            break;
+
+        case "/":
+            result = alert(a / b);
+            break;
+            
+        default:
+            result = alert("NaN");
+            break;    
+
+    }
+}
